df4fb6d2b5024a01625c02e3099c0859dcffd76a
Lfoisveer <hfr50808@gmail.com>
2023-10-14 22:46:41 -0700

    Initial Commit

---
+++ testCommit/testCommitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testCommitApp.swift
+//  testCommit
+//
+//  Created by Bobo on 10/14/23.
+//
+
+import SwiftUI
+
+@main
+struct testCommitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
